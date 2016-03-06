9ab04254b1bd3f390a32f6ea171b4c4003e21b9b
Irvan Aditya <itockz@gmail.com>
2016-03-06 17:25:35 +0700

    add favicon

--- server/index.jsx
+++ server/index.jsx
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from 'path'
+import favicon from 'serve-favicon'
 import React from 'react'
 import thunkMiddleware from 'redux-thunk'
 import {createStore, applyMiddleware} from 'redux'
@@ -12,6 +13,7 @@ import reducers from 'reducers'
 
 const app = express()
 
+app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use((req, res, next) => {
